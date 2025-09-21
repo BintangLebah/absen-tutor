5894d38fb62d7346a214c55ae03ea5d68f470ee1
BintangLebah <bee.ntanglebah@gmail.com>
2025-09-21 21:27:23 +0700

    menambah import botstrap-vue-3

--- src/main.js
+++ src/main.js
@@ -7,3 +7,15 @@ const app = createApp(App)
 app.use(router)
 
 app.mount('#app')
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import BootstrapVue3 from 'bootstrap-vue-3'
+
+// Import Bootstrap & BootstrapVue CSS
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+
+const apl = createApp(App)
+app.use(BootstrapVue3)
+app.mount('#app')
